4422b1ed9a0930b04ed913e9efacc2923f4230fa
devRenatoRodrigues <rehrlz@hotmail.com>
2023-08-10 22:03:36 -0300

    add vetors

--- portfolio-react/src/components/Header.tsx
+++ portfolio-react/src/components/Header.tsx
@@ -1,11 +1,15 @@
-import Logotipo from '../assets/Logotipo.png'
+import { Link } from 'react-router-dom'
+import Logotipo from '../assets/Logotipo.svg'
+import Back from '../assets/back.svg'
 import '../styles/Global.css'
 
 export default function Header() {
     return (
         <header className="header-container">
-
             <img className="logo" src={Logotipo} alt="logo" />
+            <Link to="/" >
+                <img className="back-arrow" src={Back} alt="logo" />
+            </Link>
         </header>
     )
 }
--- portfolio-react/src/pages/Home.tsx
+++ portfolio-react/src/pages/Home.tsx
@@ -69,6 +69,8 @@ function Home() {
                 >
                     Renato
                 </h1>)}
+
+
             </div>
             <Footer />
         </>
